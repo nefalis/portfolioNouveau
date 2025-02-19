e68b6d4ef57115e6fde47fad38c77372dc9af98d
nefalis <nefalis@gmail.com>
2025-02-19 12:15:34 +0100

    up sidebar

--- src/components/toggleBtn/ToggleBtn.jsx
+++ src/components/toggleBtn/ToggleBtn.jsx
@@ -8,7 +8,7 @@ const ToggleButton = ({ isOn, setIsOn }) => {
     };
 
     return (
-        <div className="toggleComponent mb-40">
+        <div className="toggleComponent ">
             <h2 className="text-2xl font-bold">Changement d'univers</h2>
 
             <button 
